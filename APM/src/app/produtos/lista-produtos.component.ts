d836c5e8a48021785c03af7a588379bd5f6de8ca
Danilo Silva <danilo.silva@msn.com>
2023-02-04 16:43:17 -0300

    Diretiva *ngIf

--- APM/src/app/produtos/lista-produtos.component.ts
+++ APM/src/app/produtos/lista-produtos.component.ts
@@ -6,4 +6,26 @@ import { Component } from "@angular/core";
 })
 export class ListaProdutosComponent {
     tituloPagina: string = "Lista de Produtos";
+    produtos: any[] = [
+        {
+            "id": 1,
+            "nomeProduto": "Leaf Rake",
+            "codigoProduto": "GDN-0011",
+            "dataLancamento": "March 19, 2021",
+            "descricao": "Leaf rake with 48-inch wooden handle.",
+            "preco": 19.95,
+            "classificacao": 3.2,
+            "imagem": "assets/images/leaf_rake.png"
+          },
+          {
+            "id": 2,
+            "nomeProduto": "Garden Cart",
+            "codigoProduto": "GDN-0023",
+            "dataLancamento": "March 18, 2021",
+            "descricao": "15 gallon capacity rolling garden cart",
+            "preco": 32.99,
+            "classificacao": 4.2,
+            "imagem": "assets/images/garden_cart.png"
+          }
+    ];
 }
