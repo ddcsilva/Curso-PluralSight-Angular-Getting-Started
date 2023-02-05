0f4ee0f83dda69c90e085cb04b3cfa7d8f1485b9
Danilo Silva <danilo.silva@msn.com>
2023-02-05 16:29:20 -0300

    Passagem de Parâmetros pela Rota

--- APM/src/app/produtos/detalhe-produto.component.ts
+++ APM/src/app/produtos/detalhe-produto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './detalhe-produto.component.html',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class DetalheProdutoComponent implements OnInit {
   tituloPagina: string = "Detalhes do Produto";
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+    this.tituloPagina += `: ${id}`;
   }
 
 }
